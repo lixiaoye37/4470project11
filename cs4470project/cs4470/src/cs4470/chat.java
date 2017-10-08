37f13d81ccd58104741264766b4d87ed2d2d0e51
rbcarter <r.cartercoding@gmail.com>
2017-10-07 22:37:16 -0700

    updated chat.java

--- cs4470project/cs4470/src/cs4470/chat.java
+++ cs4470project/cs4470/src/cs4470/chat.java
@@ -4,27 +4,30 @@ import java.util.Scanner;
 
 public class chat {
 
-	public static void main(String[] args) {
-		int myPort = 4545;
-		int choice = 8;
-		Scanner scanner = new Scanner(System.in);
-		
-			do{ 
-				
-			String input = scanner.nextLine(); 
-			if(input.equals("HELP")) {
-				System.out.println("myip: display IP address");
-				System.out.println("myport: my port number");
-				System.out.println("connect: connect to another peer");
-				System.out.println("send: send messages to peers");
-				System.out.println("exit: exit the program");
-			}
-			if(input.equals("exit")) {
-				choice=0;
-			}
-		}while(choice != 0);
-		System.out.print("Thanks for using <Golden eagle Chat>,Good Bye!");
+    public static void main(String[] args) {
+        int myPort = 4545;
+        int choice = 8;
+        Scanner scanner = new Scanner(System.in);
 
-	}
+        do{
+            System.out.print(">> ");
+            String input = scanner.nextLine();
+            if(input.equals("HELP")) {
+                System.out.println("myip: display my IP address");
+                System.out.println("myport: my listening port number");
+                System.out.println("connect <destination> <port no>: connect to IP address at port");
+                System.out.println("list: show all connections");
+                System.out.println("terminate <connection id>: end connection with specific id");
+                System.out.println("send <connection id> <message>: send message to id");
+                System.out.println("exit: close all connections and exit the program");
+            }
+            if(input.equals("exit")) {
+                choice=0;
+            }
+
+        }while(choice != 0);
+        System.out.print("Thanks for using <Golden eagle Chat>, Good Bye!");
+
+    }
 
 }
