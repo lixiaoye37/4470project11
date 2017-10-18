fdada60aab26fb40ae3544c5bc302b85968c6205
rbcarter <r.cartercoding@gmail.com>
2017-10-18 01:25:25 -0700

    implemented myip, myport

--- cs4470project/cs4470/src/cs4470/chat.java
+++ cs4470project/cs4470/src/cs4470/chat.java
@@ -1,18 +1,24 @@
 package cs4470;
 
 import java.util.Scanner;
+import java.net.*;
 
 public class chat {
 
     public static void main(String[] args) {
-        int myPort = 4545;
+        int port = 4545;
         int choice = 8;
         Scanner scanner = new Scanner(System.in);
+        try {
+            ServerSocket sock = new ServerSocket(port);
+        } catch(Exception e) {
+            System.out.println("Uh oh. Something happened.");
+        }
 
         do{
             System.out.print(">> ");
             String input = scanner.nextLine();
-            if(input.equals("HELP")) {
+            if(input.equals("help")) {
                 System.out.println("myip: display my IP address");
                 System.out.println("myport: my listening port number");
                 System.out.println("connect <destination> <port no>: connect to IP address at port");
@@ -21,13 +27,40 @@ public class chat {
                 System.out.println("send <connection id> <message>: send message to id");
                 System.out.println("exit: close all connections and exit the program");
             }
-            if(input.equals("exit")) {
+            else if(input.equals("myip")) {
+                System.out.println(myIP());
+            }
+            else if(input.equals("myport")) {
+                System.out.println(myPort(port));
+            }
+            else if(input.startsWith("connect")) {
+                
+            }
+            else if(input.equals("exit")) {
                 choice=0;
             }
 
         }while(choice != 0);
         System.out.print("Thanks for using <Golden eagle Chat>, Good Bye!");
 
+    }
+
+    public static void connect(String arguments) {
+
+    }
+
+    public static String myIP() {
+        try {
+            InetAddress IP = InetAddress.getLocalHost();
+            return IP.getHostAddress();
+        } catch(Exception e) {
+            System.out.println("Uh oh. Something happened.");
+        }
+        return ""; //this happens if something breaks
+    }
+
+    public static int myPort(int port) {
+        return port;
     }
 
 }
