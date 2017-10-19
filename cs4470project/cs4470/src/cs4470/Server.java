486b2cfc383aaa92217461d6ba74e3878cf8ca87
rbcarter <r.cartercoding@gmail.com>
2017-10-18 19:13:43 -0700

    initial commit

---
+++ cs4470project/cs4470/src/cs4470/Server.java
@@ -0,0 +1,14 @@
+package cs4470;
+
+public class Server implements Runnable{
+    private int port;
+
+
+    public Server(int port) {
+
+    }
+
+    public void run() {
+        //do stuff
+    }
+}
